7e11c879e15c282c3af5f6967a09517989c12cc8
hangoctienvn <59224960+hangoctienvn@users.noreply.github.com>
2021-05-12 22:29:20 +0700

    Update ApiManager.java

--- app/src/main/java/com/example/appweather/network/ApiManager.java
+++ app/src/main/java/com/example/appweather/network/ApiManager.java
@@ -9,11 +9,11 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiManager {
-    public static String BASE_URL = "https://dataservice.accuweather.com/";
+    public static String BASE_URL = "https://dataservice.accuweather.com";
 
-    @GET("http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/353412?apikey=93Qg780lHwYM4SO58n7DFPLqHg4oKADn&language=vi-vn&metric=true")
+    @GET("/forecasts/v1/hourly/12hour/353412?apikey=93Qg780lHwYM4SO58n7DFPLqHg4oKADn&language=vi-vn&metric=true")
     Call<List<Weather>> gethour();
 
-    @GET("http://dataservice.accuweather.com/forecasts/v1/daily/5day/353412?apikey=tbFOLXfZmAxAexEYOmXhcxnbZBDjQBSh&language=vi-vn&metric=true")
+    @GET("/forecasts/v1/daily/5day/353412?apikey=tbFOLXfZmAxAexEYOmXhcxnbZBDjQBSh&language=vi-vn&metric=true")
     Call<List<Weather>> getDay();
 }
